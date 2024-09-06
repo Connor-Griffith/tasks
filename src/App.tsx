2616e3cb1d5b63f7a0da502dfb455535779cff82
Connor Griffith <congriff@udel.edu>
2024-09-05 21:06:25 -0400

    Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./App.css";
+import { Button, Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,6 +10,51 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript Connor Griffith
             </header>
+            <h1>New Header</h1>
+            <div>
+                <Container>
+                    <Row>
+                        <Col
+                            style={{
+                                height: 200,
+                                width: 150,
+                                backgroundColor: "red",
+                            }}
+                        ></Col>
+                        <Col
+                            styl={{
+                                height: 200,
+                                width: 150,
+                                backgroundColor: "red",
+                            }}
+                        ></Col>
+                        <Col
+                            styl={{
+                                height: 200,
+                                width: 150,
+                                backgroundColor: "red",
+                            }}
+                        ></Col>
+                    </Row>
+                </Container>
+            </div>
+            <ol>
+                <li>First thing</li>
+                <li>Next thing</li>
+                <li>Last thing</li>
+            </ol>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                {" "}
+                Log Hello World
+            </Button>
+            <img
+                src="https://cdn.mos.cms.futurecdn.net/ASHH5bDmsp6wnK6mEfZdcU.jpg"
+                alt="A picture of a dog"
+            />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
