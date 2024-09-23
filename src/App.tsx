62fd54e1bcc176d202b280b52f9734e5a5a3ded4
Connor Griffith <congriff@udel.edu>
2024-09-23 15:20:27 -0400

    Redoing nested and objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
-import { Button, Col } from "react-bootstrap";
-import { Row } from "react-bootstrap";
-import { Container } from "react-bootstrap";
+import { ChangeType } from "./components/ChangeType";
+import { RevealAnswer } from "./components/RevealAnswer";
+import { StartAttempt } from "./components/StartAttempt";
+import { TwoDice } from "./components/TwoDice";
+import { CycleHoliday } from "./components/CycleHoliday";
+import { Counter } from "./components/Counter";
 
 function App(): React.JSX.Element {
     return (
@@ -10,55 +13,18 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript Connor Griffith
             </header>
-            <h1>New Header</h1>
-            <div>
-                <Container>
-                    <Row>
-                        <Col
-                            style={{
-                                height: 200,
-                                width: 150,
-                                backgroundColor: "red",
-                            }}
-                        ></Col>
-                        <Col
-                            styl={{
-                                height: 200,
-                                width: 150,
-                                backgroundColor: "red",
-                            }}
-                        ></Col>
-                        <Col
-                            styl={{
-                                height: 200,
-                                width: 150,
-                                backgroundColor: "red",
-                            }}
-                        ></Col>
-                    </Row>
-                </Container>
-            </div>
-            <ol>
-                <li>First thing</li>
-                <li>Next thing</li>
-                <li>Last thing</li>
-            </ol>
-            <Button
-                onClick={() => {
-                    console.log("Hello World!");
-                }}
-            >
-                {" "}
-                Log Hello World
-            </Button>
-            <img
-                src="https://cdn.mos.cms.futurecdn.net/ASHH5bDmsp6wnK6mEfZdcU.jpg"
-                alt="A picture of a dog"
-            />
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Hello World
-            </p>
+            <hr></hr>
+            <Counter></Counter>
+            <hr />
+            <RevealAnswer></RevealAnswer>
+            <hr />
+            <StartAttempt></StartAttempt>
+            <hr />
+            <TwoDice></TwoDice>
+            <hr />
+            <ChangeType></ChangeType>
+            <hr />
+            <CycleHoliday></CycleHoliday>
         </div>
     );
 }
